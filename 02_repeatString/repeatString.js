4d75ab0a015d816bf249c71e3b653319197aee04
zarriy <zarriy@outlook.com>
2022-07-26 19:10:23 +0500

    passed the 'repeatString' test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, n) {
+  let fin;
+  if (n < 0) return "ERROR";
+  for (let x = 0; x <= n; x++) {
+    fin = str.repeat(x);
+  }
+  return fin;
 };
 
 // Do not edit below this line
