a6ac6eaf9bebcc745bc7499994af365427510f82
zarriy <zarriy@outlook.com>
2022-07-29 16:20:26 +0500

    passing all tests for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fer) {
+  const convTOCenti = ((fer - 32) * 5) / 9;
+  return Math.round(convTOCenti * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (centi) {
+  const convtoFer = (centi * 9) / 5 + 32;
+  return Math.round(convtoFer * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
