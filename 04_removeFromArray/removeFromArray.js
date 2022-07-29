b984412926dc5e8abdcf69506383e774cc8a3df8
zarriy <zarriy@outlook.com>
2022-07-29 11:33:42 +0500

    Passed all tests for removing element from an array using splice method.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...num) {
+  //let's first use splice method.
+  for (let x = 0; x < arr.length; x++) {
+    for (let i = 0; i < num.length; i++) {
+      if (arr[x] === num[i]) {
+        arr.splice(x, 1);
+        x--;
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
