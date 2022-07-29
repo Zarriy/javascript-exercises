15f31ad5693974558c365e59983a9e23cee24d5a
zarriy <zarriy@outlook.com>
2022-07-29 12:36:12 +0500

    passing the test for sum all ecample

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  let sum = 0;
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
 
+  for (let x = min; x <= max; x++) {
+    sum += x;
+  }
+  return sum;
 };
 
 // Do not edit below this line
