623738875ef65e4dc8c285161d22c7ad9ca48222
zarriy <zarriy@outlook.com>
2022-08-02 22:18:01 +0500

    passed the tests of getting the title and finding the oldest'

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const thisYear = new Date().getFullYear();
+  const arrange = people
+    .map((person) => {
+      person.age = person.yearOfDeath
+        ? person.yearOfDeath - person.yearOfBirth
+        : thisYear - person.yearOfBirth;
+      return person;
+    })
+    .sort((a, b) => b.age - a.age);
+  return arrange[0];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
