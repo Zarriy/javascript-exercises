91daeb583fd032f323d524e2934dcbd3c9328783
zarriy <zarriy@outlook.com>
2022-08-02 21:33:52 +0500

    commiting the palindromes and fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
-};
+const palindromes = function (value) {
+    const para = value.toLowerCase().replace(/[^a-z]/g, "");
+    const reversepara = para.split("").reverse().join("");
+  
+    return para === reversepara;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (value) {
+  // 1 + 1 + 2 + 3
+  if (value < 0) return "OOPS";
+  let n1 = 1;
+  let n2 = 0;
+  let n = 0;
+  for (let x = 1; x <= value; x++) {
+    n = n1 + n2;
+    n1 = n2;
+    n2 = n;
+  }
+  return n;
 };
 
 // Do not edit below this line
